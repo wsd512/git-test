78d813ca5f72caef43b7d7961bbc6fff48a22361
wsd <292467402@qq.com>
2022-02-20 18:34:06 +0800

    首次提交

---
+++ src/main/java/com/wsd/GitTest.java
@@ -0,0 +1,15 @@
+package com.wsd;
+
+/**
+ * @program: wsd165
+ * @description
+ * @author: wansidong
+ * @create: 2022-02-20 18:30
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
